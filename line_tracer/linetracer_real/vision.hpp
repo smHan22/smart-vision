2cdfd1b9fc4943e518df3b91965242885c017dc2
SMHAN22 <90818408+smHan22@users.noreply.github.com>
2024-12-10 20:36:56 +0900

    Update vision.hpp

--- line_tracer/linetracer_real/vision.hpp
+++ line_tracer/linetracer_real/vision.hpp
@@ -1,1 +1,16 @@
+#ifndef VISION_HPP
+#define VISION_HPP
 
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+// 전처리 함수
+void preprocessFrame(const Mat& frame, Mat& binary, Mat& colorBinary);
+
+// 라인 중심 계산 및 에러 반환 함수
+double calculateError(const Mat& binary, Mat& colorBinary, Point& previousCenter, bool& firstFrame, double maxDistance, double error);
+
+#endif 
